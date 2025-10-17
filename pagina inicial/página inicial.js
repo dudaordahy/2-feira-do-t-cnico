43195146042134f3b931c07d56c5ebfb71fa3c48
dudaordahy <dudasciortino@gmail.com>
2025-10-17 13:58:14 -0300

    atualizações

--- pagina inicial/página inicial.js
+++ pagina inicial/página inicial.js
@@ -21,7 +21,7 @@ console.log(colecaoPreferencias);
 
 let container = document.getElementById('container_escolhas');
 let txt_container = document.getElementById('txt_escolha_perfil');
-let btn_foto = document.getElementById('btn_foto');
+let btn_foto = document.getElementById('btn_foto_perfil');
 
 window.sr = ScrollReveal({reset: true});
 sr.reveal(container, {duration: 2500});
@@ -48,70 +48,70 @@ function proximaPagina(){
   document.getElementById("container_escolhas").appendChild(texto);
 
   const div_botoes = document.createElement("div")
-  div_botoes.id = "container_botoes";
+  div_botoes.id = "container_botoes_preferencias";
   document.getElementById("container_escolhas").appendChild(div_botoes);
 
   const botao = document.createElement("button");
-  botao.className = "btns_escolhas";  
-  document.getElementById("container_botoes").appendChild(botao);
+  botao.className = "btns_preferencias";  
+  document.getElementById("container_botoes_preferencias").appendChild(botao);
 
   const botao1 = document.createElement("button");
-  botao1.className = "btns_escolhas";  
-  document.getElementById("container_botoes").appendChild(botao1);
+  botao1.className = "btns_preferencias";  
+  document.getElementById("container_botoes_preferencias").appendChild(botao1);
 
   const botao2 = document.createElement("button");
-  botao2.className = "btns_escolhas";  
-  document.getElementById("container_botoes").appendChild(botao2);
+  botao2.className = "btns_preferencias";  
+  document.getElementById("container_botoes_preferencias").appendChild(botao2);
 
   const botao3 = document.createElement("button");
-  botao3.className = "btns_escolhas";  
-  document.getElementById("container_botoes").appendChild(botao3);
+  botao3.className = "btns_preferencias";  
+  document.getElementById("container_botoes_preferencias").appendChild(botao3);
 
   const botao4 = document.createElement("button");
-  botao4.className = "btns_escolhas";  
-  document.getElementById("container_botoes").appendChild(botao4);
+  botao4.className = "btns_preferencias";  
+  document.getElementById("container_botoes_preferencias").appendChild(botao4);
   
   const botao5 = document.createElement("button");
-  botao5.className = "btns_escolhas";  
-  document.getElementById("container_botoes").appendChild(botao5);
+  botao5.className = "btns_preferencias";  
+  document.getElementById("container_botoes_preferencias").appendChild(botao5);
 
   const botao6 = document.createElement("button");
-  botao6.className = "btns_escolhas";  
-  document.getElementById("container_botoes").appendChild(botao6);
+  botao6.className = "btns_preferencias";  
+  document.getElementById("container_botoes_preferencias").appendChild(botao6);
 
   const botao7 = document.createElement("button");
-  botao7.className = "btns_escolhas";  
-  document.getElementById("container_botoes").appendChild(botao7);
+  botao7.className = "btns_preferencias";  
+  document.getElementById("container_botoes_preferencias").appendChild(botao7);
 
   const botao8 = document.createElement("button");
-  botao8.className = "btns_escolhas";  
-  document.getElementById("container_botoes").appendChild(botao8);
+  botao8.className = "btns_preferencias";  
+  document.getElementById("container_botoes_preferencias").appendChild(botao8);
 
   const botao9 = document.createElement("button");
-  botao9.className = "btns_escolhas";  
-  document.getElementById("container_botoes").appendChild(botao9);
+  botao9.className = "btns_preferencias";  
+  document.getElementById("container_botoes_preferencias").appendChild(botao9);
 
   const botao10 = document.createElement("button");
-  botao10.className = "btns_escolhas";  
-  document.getElementById("container_botoes").appendChild(botao10);
+  botao10.className = "btns_preferencias";  
+  document.getElementById("container_botoes_preferencias").appendChild(botao10);
 
   const botao11 = document.createElement("button");
-  botao11.className = "btns_escolhas";  
-  document.getElementById("container_botoes").appendChild(botao11);
+  botao11.className = "btns_preferencias";  
+  document.getElementById("container_botoes_preferencias").appendChild(botao11);
 
   const botao12 = document.createElement("button");
-  botao12.className = "btns_escolhas";  
-  document.getElementById("container_botoes").appendChild(botao12);
+  botao12.className = "btns_preferencias";  
+  document.getElementById("container_botoes_preferencias").appendChild(botao12);
 
   const botao13 = document.createElement("button");
-  botao13.className = "btns_escolhas";  
-  document.getElementById("container_botoes").appendChild(botao13);
+  botao13.className = "btns_preferencias";  
+  document.getElementById("container_botoes_preferencias").appendChild(botao13);
 
   const botao14 = document.createElement("button");
-  botao14.className = "btns_escolhas";  
-  document.getElementById("container_botoes").appendChild(botao14);
+  botao14.className = "btns_preferencias";  
+  document.getElementById("container_botoes_preferencias").appendChild(botao14);
 
   const botao15 = document.createElement("button");
-  botao15.className = "btns_escolhas";  
-  document.getElementById("container_botoes").appendChild(botao15);
+  botao15.className = "btns_preferencias";  
+  document.getElementById("container_botoes_preferencias").appendChild(botao15);
 }
