57d105393df27a5c028ec620e1263d841015df79
dudaordahy <dudasciortino@gmail.com>
2025-10-24 08:59:28 -0300

    atualizacao

--- includes/header.php
+++ includes/header.php
@@ -3,8 +3,8 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Connect LEGAL</title>
+    <title>Connect</title>
     <link rel="stylesheet" href="./assets/css/template.css">
-    
+    <link rel="shortcut icon" href="assets/img/logo.png" type="image/x-icon">
 </head>
 <body>
